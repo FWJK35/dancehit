06797651765426144fe44964a28dc49aa09205a4
FWJK35 <calvinrodrigue@gmail.com>
2025-02-02 04:45:38 -0500

    Clicking on song sets beat map

--- client/src/components/modules/SongsList.jsx
+++ client/src/components/modules/SongsList.jsx
@@ -1,10 +1,20 @@
+import React from "react";
+import { setBeatMap } from "../../game-logic";
+
 const SongsList = (props) => {
   return (
     <div>
       <h2>Songs List</h2>
       <ul>
         {props.songs.map((song) => (
-          <li key={song._id}>{song.name}</li>
+          <li
+            key={song._id}
+            onClick={() => {
+              setBeatMap(song.beats);
+            }}
+          >
+            {song.name}
+          </li>
         ))}
       </ul>
     </div>
